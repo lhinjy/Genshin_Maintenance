4993d9e3516516d96ed030d9eb8fe5bb58c5c467
lhinjy <jiayinjyjy@gmail.com>
2021-10-12 21:59:52 +0800

    Link to github works locally but not on url (hopefully fixed with this commit)

--- main.py
+++ main.py
@@ -64,11 +64,11 @@ def write_disclaimer(text):
         }
         </style>
         """, unsafe_allow_html=True)
-    st.markdown(f'<p class="custom_footer">{text}', unsafe_allow_html=True)
+    st.markdown(f'<p class="custom_footer">{text}</p>', unsafe_allow_html=True)
 
 
 def main():
-    write_disclaimer("This is not an official tool by MiHoYo. Source code is available on <a href ='https://github.com/lhinjy/Genshin_Maintenance'>github</a></p>'")
+    write_disclaimer("This is not an official tool by MiHoYo. Source code is available on <a href ='https://github.com/lhinjy/Genshin_Maintenance'>github</a>")
 
     write_custom_text("Genshin Impact 2.2 Maintenance Starts At:")
     country_chosen,country_button, utc_timezone_chosen, utc_timezone_button = display_user_input()
